9d975e098753aa3f389d524e93ecd975eeac01c0
h4meru <127676398+h4meru@users.noreply.github.com>
2023-03-13 11:16:26 +0800

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,20 @@
+var mof2 = 2;
+while (mof2 <= 60) {
+    document.getElementById("mof2").innerHTML = document.getElementById("mof2").innerHTML + " " + mof2;
+    mof2 = mof2 + 2;
+}
+
+///////////
+
+var mof3 = 3;
+do {
+    document.getElementById("mof3").innerHTML = document.getElementById("mof3").innerHTML + " " + mof3;
+    mof3 = mof3 + 3;
+}
+while (mof3 <= 60);
+
+//////////
+
+for (var mof5 = 5; mof5 <= 50; mof5 = mof5 + 5) {
+    document.getElementById("mof5").innerHTML = document.getElementById("mof5").innerHTML + " " + mof5;
+}
